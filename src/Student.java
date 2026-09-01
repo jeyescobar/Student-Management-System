70b9edd66bd2a2c080e78ed4a86b5ac2e4f93fac
jeyescobarrivera2 <jeymiguel2@gmail.com>
2026-08-31 22:35:00 -0400

    Student Constructor, Getters and Setters implemented

---
+++ src/Student.java
@@ -0,0 +1,41 @@
+public class Student {
+    private int id;
+    private String name;
+    private String major;
+    private double gpa;
+
+    public Student(int id, String name, String major, double gpa){
+        this.id = id;
+        this.name = name;
+        this.major = major;
+        this.gpa = gpa;
+    }
+
+    //getters
+
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getMajor(){
+        return major;
+    }
+    public double getGpa(){
+        return gpa;
+    }
+
+    //setters
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setMajor(String major){
+        this.major = major;
+    }
+    public void setGpa(double gpa){
+        this.gpa = gpa;
+    }
+
+}
