029e2f47b7443dfec8fcaa0243296261f2317014
jeyescobarrivera2 <jeymiguel2@gmail.com>
2026-08-31 21:40:51 -0400

    Add Initial Menu

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String [] args){
+        printMenu();
+    }
+    public static void printMenu(){
+        System.out.println("===== STUDENT MANAGMENT SYSTEM ===== \n" +
+                "1. Add Student \n"+
+                "2. View Students \n"+
+                "3. Search Student \n"+
+                "4. Update Student\n"+
+                "5. Delete Student\n"+
+                "6. Exit\n \n"+
+                "Select an option: ");
+    }
+}
